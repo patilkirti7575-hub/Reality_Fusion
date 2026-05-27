0abc211a182e01039b898a31e8dadda0fc303604
Kirti Khairnar <patilkirti7575@gmail.com>
2026-05-27 13:32:40 +0530

    update

--- RealityFusion_project/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.NotificationListView.as_view(), name='notifications'),
-    path('count/', views.NotificationCountView.as_view(), name='notification_count'),
-    path('<int:notification_id>/read/', views.NotificationMarkReadView.as_view(), name='notification_mark_read'),
-    path('read-all/', views.NotificationMarkAllReadView.as_view(), name='notification_mark_all_read'),
-]
